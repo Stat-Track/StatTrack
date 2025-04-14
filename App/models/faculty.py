cac6c34f1db1a22a7c74bb0a9905cd9034e16ce2
K-Baksh <157691631+K-Baksh@users.noreply.github.com>
2025-04-14 19:10:23 -0400

    Update Faculty Model

--- App/models/faculty.py
+++ App/models/faculty.py
@@ -1,7 +1,18 @@
 from App.database import db
 
 class Faculty(db.Model):
-    facultyId = db.Column(db.Integer, primary_key=True)
-    facultyName = db.Column(db.String(80), nullable=False)
-    campusId = db.Column(db.Integer, db.ForeignKey('campus.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    campus_id = db.Column(db.Integer, db.ForeignKey('campus.id'), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'<Campus - {self.campus.name}, Faculty {self.campusId} - {self.campusName}>'
+    
+    def get_json(self):
+        "id" = self.id
+        "name" = self.name
+        "campus_id" = self.campus_id
 
