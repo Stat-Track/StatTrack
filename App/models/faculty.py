f99434cc9a63ec35c2180574e3920e839a91ab28
K-Baksh <157691631+K-Baksh@users.noreply.github.com>
2025-04-14 19:13:39 -0400

    Establish relationship between Faculty and Report

--- App/models/faculty.py
+++ App/models/faculty.py
@@ -4,6 +4,7 @@ class Faculty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     campus_id = db.Column(db.Integer, db.ForeignKey('campus.id'), nullable=False)
+    reports = db.relationship('Report', backref='faculty', lazy=True, cascade="all, delete-orphan")
 
     def __init__(self, name):
         self.name = name
--- App/models/report.py
+++ App/models/report.py
@@ -5,4 +5,5 @@ class Report(db.Model):
     reportName = db.Column(db.String(255), nullable=False)
     image = db.Column(db.String(255), nullable=False)
     text = db.Column(db.String(255), nullable=False)
+    faculty_id = db.Column(db.Integer, db.ForeignKey('faculty.id'), nullable=False)
 
