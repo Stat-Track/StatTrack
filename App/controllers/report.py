357d7dec991128d254b2fe7afae97f07e994296c
K-Baksh <157691631+K-Baksh@users.noreply.github.com>
2025-04-18 07:46:25 -0400

    Add delete functions to report controller

--- App/controllers/report.py
+++ App/controllers/report.py
@@ -6,3 +6,23 @@ def create_report(file):
     db.session.add(new_report)
     db.session.commit()
     return new_report
+
+def delete_report_by_id(id):
+    report = Report.query.get(id)
+    if not report:
+      print(f'Report {id} not found!')
+      return
+    report.remove_file(report.filename)
+    db.session.delete(report)
+    db.session.commit()
+    print(f'Report {id} deleted')
+
+def delete_report_by_filename(filename):
+    report = Report.query.filter_by(filename=filename).first()
+    if not report:
+      print(f'Report {filename} not found!')
+      return
+    report.remove_file(report.filename)
+    db.session.delete(report)
+    db.session.commit()
+    print(f'Report {filename} deleted')
