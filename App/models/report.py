0264f8d3b5f1f6fb1a6e183e2358f55ee3d48fa2
K-Baksh <157691631+K-Baksh@users.noreply.github.com>
2025-04-18 04:09:00 -0400

    Update Report model

--- App/models/report.py
+++ App/models/report.py
@@ -1,11 +1,31 @@
 from App.database import db
+import os,string, random
+from werkzeug.utils import secure_filename
 
 class Report(db.Model):
-    reportId = db.Column(db.Integer, primary_key=True)
-    reportName = db.Column(db.String(255), nullable=False)
-    image = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.String(255), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
     faculty_id = db.Column(db.Integer, db.ForeignKey('faculty.id'), nullable=False)
+
+    def __init__(self, file):
+        self.filename = self.store_file(file)
+
+    def random_string():
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+
+    def remove_file(filename):
+        try:
+            os.remove(os.path.join('App/reports', filename))
+        except:
+            print('file already Deleted')
     
+    def get_url(self):
+        return f'/reports/{self.filename}'
+    
+    def store_file(self, file):
+        extension = os.path.splitext(file.filename)[1]
+        newname = secure_filename(self.random_string() + extension)
+        file.save(os.path.join('reports', newname))
+        return newname
 
