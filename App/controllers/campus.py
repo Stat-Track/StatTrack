0e46625d0e957b559bde11a30d095cb5e2d4b14d
Shania <shaniasiew14@gmail.com>
2025-04-21 03:02:17 -0400

    Faculty and Campus functions

--- App/controllers/campus.py
+++ App/controllers/campus.py
@@ -1,4 +1,5 @@
 from App.models import Campus
+from App.models import Report
 from App.database import db
 
 def create_campus(name):
@@ -12,3 +13,42 @@ def get_all_campuses():
 
 def get_campus_by_id(campus_id):
     return Campus.query.get(campus_id)
+
+def update_campus(campus_id, name):
+    campus = get_campus_by_id(campus_id)
+    if campus:
+        campus.name = name
+        db.session.commit()
+        return campus
+    return None
+
+def delete_campus(campus_id):
+    campus = get_campus_by_id(campus_id)
+    if campus:
+        db.session.delete(campus)
+        db.session.commit()
+        return True
+    return False
+
+
+def get_campus_by_name(name):
+    return Campus.query.filter_by(name=name).first()
+
+def validate_campus(campus_id):
+    return Campus.query.filter_by(id=campus_id).first() is not None
+
+def get_reports_by_campus_and_filter(campus_id, **filters):
+    query = Report.query.filter_by(campus_id=campus_id)
+    for key, value in filters.items():
+        query = query.filter(getattr(Report, key) == value)
+    return query.all()
+
+def get_campus_dropdown():
+    return [{"id": campus.id, "name": campus.name} for campus in get_all_campuses()]
+
+def get_campus_reports_with_faculty(campus_id, faculty_id=None):
+    query = Report.query.filter_by(campus_id=campus_id)
+    if faculty_id:
+        query = query.filter_by(faculty_id=faculty_id)
+    return query.all()
+
--- App/controllers/faculties.py
+++ App/controllers/faculties.py
@@ -1,5 +1,6 @@
 from App.models import Faculty
 from App.database import db
+from App.models import Report
 
 def create_faculty(name, campus_id):
     faculty = Faculty(name=name, campus_id=campus_id)
@@ -15,3 +16,32 @@ def get_faculties_by_campus(campus_id):
 
 def get_faculty_by_id(faculty_id):
     return Faculty.query.get(faculty_id)
+
+def update_faculty(faculty_id, name, campus_id):
+    faculty = get_faculty_by_id(faculty_id)
+    if faculty:
+        faculty.name = name
+        faculty.campus_id = campus_id
+        db.session.commit()
+        return faculty
+    return None
+
+def delete_faculty(faculty_id):
+    faculty = get_faculty_by_id(faculty_id)
+    if faculty:
+        db.session.delete(faculty)
+        db.session.commit()
+        return True
+    return False
+
+def get_faculty_reports(faculty_id):
+    faculty = get_faculty_by_id(faculty_id)
+    if faculty:
+        return faculty.reports.all()
+    return None
+
+def validate_faculty(faculty_id):
+    return Faculty.query.get(faculty_id) is not None
+
+def get_reports_by_campus_and_faculty(campus_id, faculty_id):
+    return Report.query.filter_by(campus_id=campus_id, faculty_id=faculty_id).all()
